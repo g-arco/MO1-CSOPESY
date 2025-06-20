1c53da7ca45fd996b7915837b76e70d3f5ec7ece
Roger Canayon Jr. <rcanayonjr@gmail.com>
2025-06-20 09:43:57 +0800

    Revise CLIUtils

--- TEST-MO1/CLIUtils.cpp
+++ TEST-MO1/CLIUtils.cpp
@@ -15,5 +15,12 @@ void CLIUtils::printHeader() {
     std::cout << "| |    | |___ | |  | || |_| || |__ | |____   \\ \\/ / \n";
     std::cout << "| |____ ___| || |  | || ___/ | ___|  ___| |   |  |  \n";
     std::cout << " \\____| ____/ \\ \\__/ /|_|    |____| |____/    |__|  \n";
-    std::cout << "\033[32mWelcome to the simulated Linux Screen CLI (C++)\033[0m\n\n";
+    std::cout << "----------------------------------------------------\n";
+    std::cout << "\033[32mWelcome to CSOPESY Emulator!\033[0m\n\n";
+    std::cout << "\033[32mDevelopers:\033[0m\n\n";
+    std::cout << "  - \033[34mArco, Gabrielle Mae\033[0m\n";
+    std::cout << "  - \033[34mCanayon Jr., Roger\033[0m\n";
+    std::cout << "  - \033[34mDe Los Reyes, Carl Justin\033[0m\n";
+    std::cout << "  - \033[34mViguilla, Andrei Dominic\033[0m\n";
+    std::cout << "----------------------------------------------------\n\n";
 }
